e41b629d28196c3db80393d72958fb5e0ca078e5
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-07-09 13:43:21 +0300

    App 'content-slot' component was created.

--- resources/assets/js/components/app/layout/content-slot.vue
+++ resources/assets/js/components/app/layout/content-slot.vue
@@ -1,3 +1,5 @@
+// @flow
+
 <template lang="pug">
 
     .CONTENT-SLOT(ref="contentSlot")
@@ -11,9 +13,14 @@
 
     .CONTENT-SLOT
         +position(r, $t:0, $l:0)
+        +border(green)
 
 </style>
 <script>
+
+    import {Height      } from "../../../classes/types/Height";
+    import {EVENTS      } from "../../../classes/enum/AppEvents";
+    import {EventObject } from "../../../classes/types/EventObject";
 
     // ------------------------------------------------------------------------
     // COMPONENT
@@ -25,16 +32,60 @@
         // PROPERTIES
         // --------------------------------------------------------------------
 
+        props: {
+            pHeight:{
+                type: Height,
+                default: function () {
+                    return null;
+                }
+            }
+        },
 
         // --------------------------------------------------------------------
         // DATA FIELDS
         // --------------------------------------------------------------------
 
+        data(){
+
+            const data: {
+                dHeight: Height,
+            } =  {
+                dHeight: new Height(0, 'px'),
+            };
+            return data;
+        },
 
         // --------------------------------------------------------------------
         // COMPUTED FIELDS
         // --------------------------------------------------------------------
 
+        computed: {
+
+            /**
+             * .CONTENT-SLOT element htight.
+             * @param {Height} newHeight
+             */
+            height: {
+                set: function (newHeight: Height) {
+
+                    const event: EventObject<Height> = new EventObject(
+                        newHeight,
+                        this
+                    );
+
+                    this.$bus.$emit(
+                        EVENTS.APP.CONTENT_SLOT.HEIGHT_CHANGED,
+                        event
+                    );
+
+                    this.dHeight          = newHeight;
+                    this.$el.style.height = newHeight.value + newHeight.measure;
+                },
+                get: function () {
+                    return this.dHeight;
+                }
+            }
+        },
 
         // --------------------------------------------------------------------
         // WATCHED FIELDS
@@ -50,6 +101,8 @@
         // LIFE HOOKS
         // --------------------------------------------------------------------
 
+        mounted(){
+        }
 
     }
 </script>

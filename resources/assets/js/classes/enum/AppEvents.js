2459fb797385cb7c7feaaf050906899daa44f4d6
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-07-09 13:38:04 +0300

    'AppEvents' class(enum) was created. Represents the list of app events names.

--- resources/assets/js/classes/enum/AppEvents.js
+++ resources/assets/js/classes/enum/AppEvents.js
@@ -1,3 +1,22 @@
+// Events counter.
+let _counter: number = 0;
+
+function _id(): string{
+    return (++_counter).toString();
+}
+
 /**
- * Created by mirtg_000 on 08.07.2017.
+ * Event names and id.
  */
+class AppEvents {
+
+    APP = Object.freeze({
+        MAIN_CONTENT_SLOT:  Object.freeze({ HEIGHT_CHANGED: _id() }),
+        TOP_NAVBAR_SLOT:    Object.freeze({ HEIGHT_CHANGED: _id() }),
+        CONTENT_SLOT:       Object.freeze({ HEIGHT_CHANGED: _id() }),
+    });
+}
+
+const EVENTS = Object.freeze(new AppEvents());
+
+export {EVENTS};

697e9a3b8b24357d82bba14c7f89b66f906792d6
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-07-09 13:26:15 +0300

    'Height' class was created. Stores information about height value of HTML element.

--- resources/assets/js/classes/types/Height.js
+++ resources/assets/js/classes/types/Height.js
@@ -1,3 +1,22 @@
-/**
- * Created by mirtg_000 on 08.07.2017.
- */
+// @flow
+
+class Height {
+
+    _value  : Number | number;
+    _measure: String | string;
+
+    constructor(height: Number | number, measure: String | string) {
+        this._value   = height;
+        this._measure = measure;
+    }
+
+    get value  (): Number | number {
+        return this._value;
+    }
+    get measure(): String | string {
+        return this._measure;
+    }
+}
+
+export {Height}
+

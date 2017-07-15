f2dc588c5815635a8e4f15256191b6525cf0d394
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-07-15 20:21:27 +0300

    Login form filds validation was added.

--- resources/assets/js/admin-app/components/pages/login/login-form.vue
+++ resources/assets/js/admin-app/components/pages/login/login-form.vue
@@ -7,13 +7,14 @@
         :label-width="settings.labelWidth",
         :model="inputs",
         :rules="rules",
+        ref="adminLoginForm"
     )
-        el-form-item.LABEL(label="Login")
+        el-form-item.LABEL(label="Login",  prop="login")
             el-input(v-model="inputs.login")
-        el-form-item(label="Password")
-            el-input(v-model="inputs.password")
+        el-form-item(label="Password", prop="password")
+            el-input(v-model="inputs.password", type="password")
         el-form-item
-            el-button.LOGIN-BUTTON(type="primary") Войти
+            el-button.LOGIN-BUTTON(type="primary", @click="submitForm()") Войти
 
 </template>
 <style lang="sass" scoped>
@@ -54,28 +55,42 @@
         // --------------------------------------------------------------------
 
         data(){
-            const data: {
-                settings: {
-                    labelPosition : string,
-                    labelWidth    : string,
-                },
-                inputs: {
-                    login         : string,
-                    password      : string,
-                },
-                rules: {
-
-                },
-            } = {
+            const data = {
                 settings: {
                     labelPosition : 'top',
                     labelWidth    : '100px',
                 },
                 inputs: {
-                    login: '',
-                    password: '',
+                    login         : '',
+                    password      : '',
                 },
                 rules: {
+                    login: [
+                        {
+                            required: true,
+                            message : 'Пожалуйста, введите логин пользователя.',
+                            trigger : 'blur, change'
+                        },
+                        {
+                            min     : 1,
+                            max     : 15,
+                            message : 'Длинна логина должна быть в пределах от 1 до 15 символов',
+                            trigger : 'blur, change'
+                        }
+                    ],
+                    password: [
+                        {
+                            required: true,
+                            message: 'Пожалуйста, введите пароль пользователя.',
+                            trigger: 'blur'
+                        },
+                        {
+                            min     : 1,
+                            max     : 15,
+                            message : 'Длинна логина должна быть в пределах от 1 до 15 символов',
+                            trigger : 'blur, change'
+                        }
+                    ],
                 }
             };
 
@@ -96,6 +111,18 @@
         // METHODS
         // --------------------------------------------------------------------
 
+        methods: {
+            submitForm() {
+                this.$refs['adminLoginForm'].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+        },
 
         // --------------------------------------------------------------------
         // LIFE HOOKS

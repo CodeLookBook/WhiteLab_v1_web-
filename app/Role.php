e3bfc40fb3475cea70c23197fc70aa39f5bdd448
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-07-16 09:21:02 +0300

    User model and Role Model relationsheap 'one to many' were added progammatically to those models.

--- app/Role.php
+++ app/Role.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    //
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to date.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+        ];
+
+    function users(){
+        return $this->hasMany('App\User', 'role_id', 'id');
+    }
 }

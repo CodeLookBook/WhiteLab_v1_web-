15812f68031434dfab9e55610a1839757201008d
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-07-25 19:46:47 +0300

    PriceConroller was created.

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class PriceController extends Controller
 {
-    //
+    public function upload(Request $request){
+
+        $file = $request->file('price');
+
+        if($file){
+            $extension = $file->extension();
+
+            if($extension == 'pdf'){
+                Storage::disk('price')->put('price.'.$extension, File::get($file));
+            } else {
+                response()->json(['wrong_file_extension'], 415);
+            }
+        }
+
+    }
+
+    public function download(){
+        if(Storage::disk('price')->exists('price.pdf')){
+            return \response()->make(Storage::disk('price')->get('price.pdf'), 200, [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'inline; filename="'.'price.pdf'.'"'
+            ]);
+
+            //return response()->file(storage_path('/storage/price').'/price.pdf');
+        }
+    }
 }
